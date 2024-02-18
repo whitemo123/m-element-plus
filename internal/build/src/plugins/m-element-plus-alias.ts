5b15d5adbb0cb0f1b422c7ffa6b4a21765763031
mobai <2813089832@qq.com>
2024-02-18 18:28:00 +0800

    增加部分打包方式

---
+++ internal/build/src/plugins/m-element-plus-alias.ts
@@ -0,0 +1,20 @@
+import { PKG_NAME, PKG_PREFIX } from '../constant'
+
+import type { Plugin } from 'rollup'
+
+export function MElementPlusAlias(): Plugin {
+  const themeChalk = 'theme-chalk'
+  const sourceThemeChalk = `${PKG_PREFIX}/${themeChalk}` as const
+  const bundleThemeChalk = `${PKG_NAME}/${themeChalk}` as const
+
+  return {
+    name: 'm-element-plus-alias-plugin',
+    resolveId(id) {
+      if (!id.startsWith(sourceThemeChalk)) return
+      return {
+        id: id.replaceAll(sourceThemeChalk, bundleThemeChalk),
+        external: 'absolute',
+      }
+    },
+  }
+}
