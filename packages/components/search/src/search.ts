b5a53a037bb0a165e1ad1191510065b72eb066a2
mobai <2813089832@qq.com>
2024-03-12 11:41:07 +0800

    部分组件开发

--- packages/components/search/src/search.ts
+++ packages/components/search/src/search.ts
@@ -1,7 +1,80 @@
-import { buildProps } from "@m-element-plus/utils";
+import { buildProps, definePropType } from "@m-element-plus/utils";
+import { ICommonColumn } from "@m-element-plus/components/common/types";
+import { useSizeProp } from "@m-element-plus/hooks";
+import { FormItemRule } from "element-plus";
 
+export interface ISearchOptionColumn extends ICommonColumn {
+  /**
+   * @description label宽度
+   */
+  labelWidth?: string;
+  /**
+   * @description 搜索排序
+   */
+  order?: number;
+  /**
+   * @description 搜索校验规则
+   */
+  rule?: FormItemRule[];
+  /**
+   * @description 栅栏宽度
+   */
+  span?: number;
+}
+
+/**
+ * @description 搜索配置
+ */
+export interface ISearchOption {
+  /**
+   * @description label宽度
+   */
+  labelWidth?: string;
+  /**
+   * @description 搜索按钮文字
+   */
+  searchBtnText?: string;
+  /**
+   * @description 搜索按钮图标
+   */
+  searchBtnIcon?: string;
+  /**
+   * @description 重置按钮文字
+   */
+  resetBtnText?: string;
+  /**
+   * @description 重置按钮图标
+   */
+  resetBtnIcon?: string;
+  /**
+   * @description 收缩展示个数
+   */
+  colIndex?: number;
+  /**
+   * @description 是否开启收缩
+   */
+  col?: boolean;
+  /**
+   * @description 搜索配置项
+   */
+  column: ISearchOptionColumn[];
+}
+
+/**
+ * @description 搜索prop
+ */
 export const searchProps = buildProps({
-
+  /**
+   * @description 尺寸大小
+   */
+  size: useSizeProp,
+  /**
+   * @description 配置项
+   */
+  option: {
+    type: definePropType<ISearchOption>(Object),
+    required: true
+  }
 })
 
 export const searchEmits = {
--- packages/components/search/src/search.vue
+++ packages/components/search/src/search.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+import { searchProps } from './search';
+
 defineOptions({
   name: "MSearch"
 })
+
+defineProps(searchProps)
 </script>
 
 <template>
   <div class="m-search-box">
-    <el-form>
+    <el-form :size="size" :inline="true">
       
     </el-form>
   </div>
